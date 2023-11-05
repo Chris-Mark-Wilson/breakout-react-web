2dd936f6e65e7632f9a3073e1777087df64a9662
bigsegs <cw.plastering.services@gmail.com>
2023-11-05 08:53:57 +0000

    working keydown

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -9,9 +9,10 @@ import { useRef,useEffect } from "react";
 export const GameScreen = () => {
     const inputRef = useRef(null)
     const [count,setCount]=useState(0)
+    const[keyState,setKeyState]=useState({});
  
     const {
-        gameOver, setGameOver,setBatprops
+        gameOver, setGameOver,setBatProps
     } = useContext(GameContext);
 
 
@@ -19,31 +20,41 @@ export const GameScreen = () => {
         inputRef.current.focus();
     }, [gameOver]);
 
+    useEffect(()=>{
 
     window.addEventListener('keydown',function(e){
-        keyState[e.key] = true;
+       setKeyState(state=>{
+        const newState={...state}
+        newState[e.key]=true;
+        return newState
+       })
     },true);    
     window.addEventListener('keyup',function(e){
-        keyState[e.key] = false;
+        setKeyState(state=>{
+            const newState={...state}
+            newState[e.key]=false;
+            return newState
+           })
     }, true);
+},[])
     useEffect(() => {
         setTimeout(() => {
             setCount(count + 1)
-        })
+        },1)
     },[count])
 
     useEffect(() => { 
         if(keyState['ArrowLeft']) {
-            setBatprops(current => {
-                return {...current, x: current.x - 5}
+            setBatProps(current => {
+                return {...current, x: current.x - 10}
             })
          }
             if(keyState['ArrowRight']) {
-                setBatprops(current => {
-                    return {...current, x: current.x + 5}
+                setBatProps(current => {
+                    return {...current, x: current.x + 10}
                 })
             }
-    }, [count])
+    }, [count,keyState])
     
     
 
